c9d22e1b81195b76356e67c2cdd9fb8abc0e89b1
Nicholas Walton <cactar65@gmail.com>
2019-10-27 21:21:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -33,11 +33,11 @@ class Artist
   end
   
   def print_songs
-    Song.all.select {|song| 
+    Song.all.select do |song| 
       if song.artist == self
         puts song.name
       end
-    }
+    end
   end
   
 end
